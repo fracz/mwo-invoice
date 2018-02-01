1a61349f74521d4ae741878cbe7728b80227361d
ryjek <ryjoryj@gmail.com>
2018-02-01 16:53:50 +0000

    Renaming methodsc/test/java/pl/edu/agh/mwo/invoice/InvoiceTest.java

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -4,12 +4,27 @@ import java.math.BigDecimal;
 
 public abstract class Product {
 	private final String name;
-
 	private final BigDecimal price;
 
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+	/*	if (name == null || name.isEmpty()) {
+			// name.equals("")
+			throw new IllegalArgumentException("Product name cannot be null");
+		}
+
+		if (price == null) {
+			// name.equals("")
+			throw new IllegalArgumentException("Price cannot be null");
+		}
+		*/
+		if (name == null || name.isEmpty() || price ==null || price.compareTo(new BigDecimal(0)) <0) { 
+			throw new IllegalArgumentException("Invalid product parameters");
+		}
+		
+	//	price.signum() == -1
+
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
