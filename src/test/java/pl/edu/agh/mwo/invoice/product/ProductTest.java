1385d6282ae5fb23566489e42b97e59c2cc57d0b
Ewa Janczykowska <janczykowska@student.agh.edu.pl>
2020-01-19 14:38:07 +0100

    Fourth product test working

--- src/test/java/pl/edu/agh/mwo/invoice/product/ProductTest.java
+++ src/test/java/pl/edu/agh/mwo/invoice/product/ProductTest.java
@@ -28,6 +28,7 @@ public class ProductTest {
 		Assert.assertThat(new BigDecimal("100"), Matchers.comparesEqualTo(product.getPrice()));
 		Assert.assertThat(new BigDecimal("0.08"), Matchers.comparesEqualTo(product.getTaxPercent()));
 	}
+	
 
 	@Test
 	public void testPriceWithTax() {
