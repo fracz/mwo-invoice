659d58d6aaf76e605da1bc9d452c91b9a0578684
Student18 <Tymbark124>
2020-01-19 09:55:19 +0100

    testProductName - it's done :)

--- src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
@@ -1,6 +1,6 @@
 package pl.edu.agh.mwo.invoice.product;
 
-import java.math.BigDecimal;
+		import java.math.BigDecimal;
 
 public class TaxFreeProduct extends Product {
 	public TaxFreeProduct(String name, BigDecimal price) {
