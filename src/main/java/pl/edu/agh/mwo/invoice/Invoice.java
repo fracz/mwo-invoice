f5e1406c6870daa6bb547b15911c938e688f0472
Aleksandra Wandzilak <aleksandra.wandzilak@fis.agh.edu.pl>
2020-01-19 12:14:24 +0100

    I should have commited step by step, but I was too focused on fixing the problems. but now all test of Invoice class work :)

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,29 +2,54 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+//	private Collection<Product> products
+	private Map<Product, Integer> products =  new HashMap<>();
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.addProduct(product, 1);
+//		if (products.containsKey(product)) {
+//			int a = products.get(product);
+//			products.put(product,a+1);
+//		}
+//		products.put(product,1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		if (products.containsKey(product)) {
+			int a = products.get(product);
+			products.put(product,quantity+1);
+		}
+		products.put(product,quantity);
 	}
 
-	public BigDecimal getSubtotal() {
-		return null;
+	public BigDecimal getNetPrice() {
+		BigDecimal sum = new BigDecimal ("0");
+		for(Product s : this.products.keySet()){
+			Integer amount = this.products.get(s);
+			BigDecimal a = new BigDecimal(amount);
+			BigDecimal price = s.getPrice();
+			sum = sum.add(price.multiply(a));
+		}
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		return this.getGrossPrice().subtract(this.getNetPrice());
 	}
 
-	public BigDecimal getTotal() {
-		return null;
+	public BigDecimal getGrossPrice() {
+		BigDecimal gross = new BigDecimal("0");
+		for(Product s : this.products.keySet()){
+			BigDecimal a = new BigDecimal(this.products.get(s));
+			BigDecimal tax = s.getPriceWithTax().subtract(s.getPrice());
+			gross = gross.add(tax.multiply(a));
+		}
+		return gross;
 	}
 }
